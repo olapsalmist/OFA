abc3f3df8a1a59ccebf7b51f600a6c6ac59910fe
Oladiti <oladiti_samuel24@yahoo.com>
2020-07-08 18:49:47 +0100

    added authentication dbcontext class

--- src/OfaFc.Data/DbContext/AuthenticationDbContext/AuthenticationDbContext.cs
+++ src/OfaFc.Data/DbContext/AuthenticationDbContext/AuthenticationDbContext.cs
@@ -1,7 +1,16 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OfaFc.Data.Entities;
+
 namespace OfaFc.Data.DbContext.AuthenticationDbContext
 {
-    public class AuthenticationDbContext
+    public class AuthenticationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext> options) 
+        : base(options)
+        {
+
+        }
         
     }
 }
---
+++ src/OfaFc.Data/Entities/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace OfaFc.Data.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+    
+}
