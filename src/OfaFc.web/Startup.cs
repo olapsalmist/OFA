7a4ff1dee78d168bb0575e0c47a7a0f928787fc1
Oladiti <oladiti_samuel24@yahoo.com>
2020-07-09 23:13:36 +0100

    added database context to startup.cs

--- src/OfaFc.web/Startup.cs
+++ src/OfaFc.web/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OfaFc.Data.DbContext.AuthenticationDbContext;
 
 namespace OfaFc.web
 {
@@ -23,6 +25,9 @@ namespace OfaFc.web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AuthenticationDbContext>(options => 
+            options.UseSqlServer(Configuration.GetConnectionString("AuthenticationConnection")));
+           
             services.AddControllersWithViews();
         }
 
